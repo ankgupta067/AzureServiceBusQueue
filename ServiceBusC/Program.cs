6d29fc9a67062a387673fb032024dc868d3a1994
Ankush Gupta <ankugu@microsoft.com>
2018-10-24 15:08:54 +0530

    Add project files.

---
+++ ServiceBusC/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionstring = "Endpoint=sb://testpc.servicebus.windows.net/;SharedAccessKeyName=acessrw;SharedAccessKey=wdFkIXeYDxrvQ3y4nXRMgAnS4iUqIiBWs73lgpWqh+0=";
+            var client = new QueueClient(connectionstring, "pcqueue");
+            client.RegisterMessageHandler(MessageHandler, exceptionhandler);
+            Console.Read();
+        }
+
+        private static Task exceptionhandler(ExceptionReceivedEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Task MessageHandler(Message message, CancellationToken arg2)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(message.Body));
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ServiceBusPC/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+
+namespace ServiceBusPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionstring = "Endpoint=sb://testpc.servicebus.windows.net/;SharedAccessKeyName=acessrw;SharedAccessKey=wdFkIXeYDxrvQ3y4nXRMgAnS4iUqIiBWs73lgpWqh+0=";
+            var client = new QueueClient(connectionstring, "pcqueue");
+            var key = "";
+            var count = 1;
+            while (key != "q")
+            {
+                var message = "Message " + count;
+                count += 1;
+                client.SendAsync(new Message(Encoding.UTF8.GetBytes(message)));
+                Console.WriteLine(message);
+                key = Console.ReadKey().Key.ToString();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
